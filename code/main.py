69989158f9745e211b2400411e2e3ede6054e3bd
Raysharo <lucaslyonbusiness@gmail.com>
2023-10-13 12:18:40 +0200

    column remover

--- code/main.py
+++ code/main.py
@@ -6,9 +6,16 @@ csv_file = "data/dataQuestionnaire.csv"
 # Chargez le fichier CSV en utilisant Pandas
 data = pd.read_csv(csv_file)
 
-# Obtenez la liste des en-têtes de colonnes
-column_headers = data.columns
+# Créer une liste de toutes les colonnes à supprimer
+columns_useless = ["Séquentiel", "SID", "Heure de soumission", "Heure de complétion", "Heure de modification", "Brouillon", "Adresse IP", "UID", "Nom d'utilisateur"]
+# Supprime les colonnes de la liste ci-dessus
+data = data.drop(columns=columns_useless)
 
-# Imprimez chaque en-tête de colonne
-for header in column_headers:
-    print(header)
+# Construire une liste contenant les noms des colonnes contenant uniquement des valeurs numériques
+numeric_columns = []
+for header in data.columns:
+    if data[header].dtype in [int, float]:
+        numeric_columns.append(header)
+
+# Imprimez la liste des colonnes contenant des valeurs numériques
+print(numeric_columns)
